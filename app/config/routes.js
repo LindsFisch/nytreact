cc2bb53360590be1e652ba0f9512842f281c379d
Lindsey Fischer <lindsfisch@gmail.com>
2017-09-03 12:18:43 -0500

    all of the issues

---
+++ app/config/routes.js
@@ -0,0 +1,11 @@
+var React = require("react");
+
+var router = require("react-router");
+
+var Route = router.Route;
+
+var Router = router.Router;
+
+var hashHistory = router.hashHistory;
+
+var IndexRoute = router.IndexRoute;
