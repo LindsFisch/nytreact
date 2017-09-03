030b34c4a8a969f861a92699cb611960914b0c2b
Lindsey Fischer <lindsfisch@gmail.com>
2017-09-03 15:30:03 -0500

    Updated components

---
+++ app/components/children/Search.js
@@ -0,0 +1,24 @@
+// Include React
+var React = require("react");
+// Including the Link component from React Router to navigate within our application without full page reloads
+var Link = require("react-router").Link;
+
+var Search = React.createClass({
+
+    // Here we render the function
+    render: function () {
+
+        return (
+
+            <div className="container">
+
+
+                {this.props.children}
+
+            </div>
+        );
+    }
+});
+
+// Export the component back for use in other files
+module.exports = Search;
