72e9aaffd32c64038ff8aae68440b91ddca2d4cd
Lindsey Fischer <lindsfisch@gmail.com>
2017-09-03 14:12:58 -0500

    Updated routing and added new components

--- app/config/routes.js
+++ app/config/routes.js
@@ -9,3 +9,32 @@ var Router = router.Router;
 var hashHistory = router.hashHistory;
 
 var IndexRoute = router.IndexRoute;
+
+var Main = require("../components/Main");
+var Search = require("../components/children/Search");
+var Saved = require("../components/children/Saved");
+var Query = require("../components/children/grandchildren/Query");
+var Result = require("../components/children/grandchildren/Result");
+
+module.exports = (
+    <Router history={hashHistory}>
+
+        <Route path = "/" component = {Main}>
+
+            <Route path="Search" component={Search}/>
+                <Route path="Query" component={Query}/>
+                <Route path="Result" component={Result}/>
+
+                <IndexRoute component={Query}/>
+
+            </Route>
+            
+            <Route path="Saved" component={Saved}/>
+
+            <IndexRoute component={Search}/>
+
+
+        </Route>
+    </Router>
+
+);
